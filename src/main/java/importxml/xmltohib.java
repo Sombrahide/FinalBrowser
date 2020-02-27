a973d47afde70400fcabdd03b5e74595e4fb8a0d
dnn98 <48447768+dnn98@users.noreply.github.com>
2020-02-27 20:50:22 +0100

    implemented javadoc

--- src/main/java/importxml/xmltohib.java
+++ src/main/java/importxml/xmltohib.java
@@ -15,7 +15,16 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+/**
+ * Class for importing the data to hibernate
+ *
+ * @author Dani Garrido Morillas
+ *
+ */
 
+/**
+ * process that imports xml and saves it on hibernate
+ */
 public class xmltohib{
     public xmltohib() {
         ConfigurationHib hib = new ConfigurationHib();
@@ -24,11 +33,18 @@ public class xmltohib{
         hib.createfromxml();
         hib.closeSession();
     }
+
     public ArrayList<literal> start() {
         Document doc = openXmlDom(new File("literals.xml"));
         ArrayList<literal> lit = getliterals(doc);
         return lit;
     }
+
+    /**
+     * gets document file
+     * @param f
+     * @return
+     */
     public static Document openXmlDom(File f) {
         Document doc=null;
         //Create DocumentBuilderFactory object
@@ -52,32 +68,11 @@ public class xmltohib{
         return doc;
     }
 
-    public static String showDoc(Document doc, String lang, String error_id){
-        NodeList nodes = doc.getElementsByTagName("literals");
-        Element element = (Element) nodes.item(0);
-
-        NodeList nodelist = element.getElementsByTagName("literal");
-        Node n;
-        String out = "";
-
-        //Loop to read every item of node list
-        for (int i=0;i<nodelist.getLength();i++){
-            //Get set of items inside CD
-            n=nodelist.item(i);
-            //Check Node type is Element
-            if (n.getNodeType()==Node.ELEMENT_NODE){
-                //Cast to Element to use getElementsByTagName method
-                Element el=(Element) n;
-                //Use .item(0).getAttributes() to get atributes;
-                if (el.hasAttribute("id") && el.getAttribute("id").equals(error_id)) {
-                    out = el.getElementsByTagName(lang).item(0).getTextContent();
-                    break;
-                }
-            }
-        }
-        return out;
-    }
-
+    /**
+     * gets one literal
+     * @param doc
+     * @return
+     */
     public static ArrayList<literal> getliterals(Document doc){
         NodeList nodes = doc.getElementsByTagName("class");
         Element element = (Element) nodes.item(0);
--- src/main/java/literals/LiteralHIB.java
+++ src/main/java/literals/LiteralHIB.java
@@ -9,16 +9,33 @@ import org.hibernate.query.Query;
 
 import mapping.literal;
 
+/**
+ * Class that retrieves strings from Hibernate
+ *
+ * @author Dani Garrido Morillas
+ *
+ */
 public class LiteralHIB{
     private SessionFactory new_session;
     private Session _session;
     public LiteralHIB() {
 
     }
+
+    /**
+     * constructor
+     * @param hib
+     */
     public LiteralHIB(ConfigurationHib hib) {
         this.new_session=hib.getNew_session();
         this._session=hib.get_session();
     }
+
+    /**
+     * selects one string by input
+     * @param keyword
+     * @return
+     */
     public literal select_one(String keyword){
         List<literal> result = (List<literal>)_session.createNativeQuery("FROM literal where str_name ").list();
         if(result.size()==0) {
@@ -26,16 +43,30 @@ public class LiteralHIB{
         }
         return result.get(0);
     }
+
+    /**
+     * selects all the data from table
+     * @return returns all the table
+     */
     public List select_all() {
         List<literal> result = (List<literal>)_session.createNativeQuery("FROM treballador").list();
         return result;
     }
+
+    /**
+     * Deletes entire table
+     */
     public void delete() {
         _session.beginTransaction();
         Query<literal> query = _session.createNativeQuery("DELETE FROM treballador");
         query.executeUpdate();
         _session.getTransaction().commit();
     }
+
+    /**
+     * inserts new one literal
+     * @param new_treb
+     */
     public void insert(literal new_treb) {
         _session.beginTransaction();
         _session.save(new_treb);
