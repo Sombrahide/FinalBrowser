f2d6515812dbc5743eb39bdff26a6d1e8ea7b214
Sombrahide <47358363+Sombrahide@users.noreply.github.com>
2020-02-27 20:17:10 +0100

    Comentarios XML corregidos
    Corregidos comentarios faltantes en las clases que administraban el manejo de XML

--- src/main/java/literals/LiteralXML.java
+++ src/main/java/literals/LiteralXML.java
@@ -38,8 +38,8 @@ public class LiteralXML {
 		return result;
 	}
 	/**
-	 * <p></p>
-	 * @return
+	 * <p><This method takes the language of the program, after which it returns the key used in the literal table so it can be used in searches/p>
+	 * @return A brief String that contains the language key, used in MySQL searches of the literal table
 	 */
 	private String getIdiomKey() {
 		IdiomType idiom = _config.getIdiom();
